62f5f90ac3630d5570b7c6e02a8c28510a2f05f0
Gyebro <gyebroszkigergely@gmail.com>
2019-02-27 16:17:21 +0100

    Added Pendulum demo

---
+++ cpp/demo/demo-pendulum.cpp
@@ -0,0 +1,23 @@
+#include "cmlib.h"
+#include "pendulum.h"
+
+using namespace cm;
+using namespace std;
+
+int main() {
+
+    double alpha = 1.0;
+    double delta = 0.2;
+    double integration_time = 0.1;
+    Pendulum pendulum(alpha, delta, integration_time);
+
+    vec2 center = {0.0, 0.0};
+    vec2 width  = {16.0*M_PI, 10.0};
+    vector<uint32_t> cells = {1400, 800};
+
+    SCM<SCMCell<uint32_t>, uint32_t, vec2> scm(center, width, cells, &pendulum);
+    scm.solve(20);
+    scm.generateImage("pendulum.jpg");
+
+    return 0;
+}
---
+++ cpp/demo/pendulum.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Gyebro on 2019-02-27.
+//
+
+#include "pendulum.h"

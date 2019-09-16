31ea693a587f587387a126bcaf6f2a3519b60690
Gyebro <gyebroszkigergely@gmail.com>
2019-09-17 01:43:16 +0200

    Added rotation map demo

---
+++ cpp/demo/demo-rotation.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+#include "cmlib.h"
+
+#include "system.h"
+#include "vec2.h"
+
+using namespace cm;
+using namespace std;
+
+class RotationMap : public cm::DynamicalSystemBase<vec2> {
+protected:
+    double a;	    /**< Rotation angle */
+    mat2 A;			/**< System matrix A */
+    /**
+     * \brief Calculates A for a given alpha
+     */
+    mat2 Rmx(double a) const {
+		mat2 R;
+		R(0,0) = cos(a);
+		R(0,1) = -sin(a);
+		R(1,0) = sin(a);
+		R(1,1) = cos(a);
+		return R;
+	}
+public:
+    /**
+     * \brief Initializes the system with its parameters
+     */
+    RotationMap(double alpha) : a(alpha), A(Rmx(a)) {
+	}
+    /**
+    * \brief Step with the rotation map
+    */
+    vec2 step(const vec2 &y0) const {
+		vec2 y1 = A*y0;
+		return y1;
+	}
+};
+
+int main() {
+
+    double alpha = 0.05; // 0.05 vs 0.5
+    RotationMap system(alpha);
+
+    vec2 center = {0, 0};
+    vec2 width  = {1000, 1000};
+    vector<uint32_t> cells = {1000, 1000};
+
+    SCM<SCMCell<uint32_t>, uint32_t, vec2> scm(center, width, cells, &system);
+    scm.solve(20);
+    scm.generateImage("scm-rotation.jpg");
+
+    return 0;
+}
